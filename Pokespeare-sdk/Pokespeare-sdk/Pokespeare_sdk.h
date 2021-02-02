360669305fd21f14602c147b9912dfa0f213f74b
Matteo Gazzato <matteo.gazzato@generali.com>
2021-02-02 22:23:28 +0100

    (WIP) Project setup - Init SDK Xcode project
    - Add Pokespeare-sdk project

---
+++ Pokespeare-sdk/Pokespeare-sdk/Pokespeare_sdk.h
@@ -0,0 +1,18 @@
+//
+//  Pokespeare_sdk.h
+//  Pokespeare-sdk
+//
+//  Created by Matteo Gazzato on 02/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pokespeare_sdk.
+FOUNDATION_EXPORT double Pokespeare_sdkVersionNumber;
+
+//! Project version string for Pokespeare_sdk.
+FOUNDATION_EXPORT const unsigned char Pokespeare_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pokespeare_sdk/PublicHeader.h>
+
+
