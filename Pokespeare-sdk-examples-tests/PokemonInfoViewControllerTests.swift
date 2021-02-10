d899ca11ce79ff74e0d7b9806917a2c0567286d1
Matteo Gazzato <matteo.gazzato@generali.com>
2021-02-10 23:18:06 +0100

    Example app - Tests
    - Add Search UI test
    - Add Favourites UI test

--- Pokespeare-sdk-examples-tests/PokemonInfoViewControllerTests.swift
+++ Pokespeare-sdk-examples-tests/PokemonInfoViewControllerTests.swift
@@ -22,5 +22,20 @@ class PokemonInfoViewControllerTests: XCTestCase {
         vc.updateUI()
         assertSnapshot(matching: vc, as: .image)
     }
+    
+    func testSearch() throws {
+        let vc = SearchWireframe().module()
+        assertSnapshot(matching: vc, as: .image)
+    }
+    
+    func testFavourites() throws {
+        let pokemon = Pokemon(name: "Charizard",
+                              description: "Charizard description",
+                              sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+                              favourite: false)
+        let vc = FavouritesWireframe().module()
+        vc.dataProvider?.favs = [pokemon]
+        assertSnapshot(matching: vc, as: .image)
+    }
 
 }
