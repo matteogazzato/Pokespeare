5af0fd8185cd8dbb63080757db61dd78f8726d23
Matteo Gazzato <matteo.gazzato@generali.com>
2021-02-10 23:24:06 +0100

    Example app - Remove mock data
    - Remove mocked data used for tests

--- Pokespeare-sdk-examples/Modules/Search/SearchInteractor.swift
+++ Pokespeare-sdk-examples/Modules/Search/SearchInteractor.swift
@@ -22,27 +22,21 @@ class SearchInteractor {
 
 extension SearchInteractor: SearchInteractorProtocol {
     func search(pokemonWithName name: String) {
-        // TODO: REMOVE
-        self.output?.handle(searchResult: Pokemon(name: "Charizard",
-                                                  description: "Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally.",
-                                                  sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"))
-        
-        
-//        pokemonSearchResult.name = name
-//        networkManager.fetchDescription(forPokemonWithName: pokemonSearchResult.name) { [weak self] (description, error) in
-//            guard let self = self else { return }
-//            if let error = error {
-//                NSLog(error.localizedDescription)
-//                self.output?.handle(searchError: error)
-//            } else {
-//                guard let description = description else {
-//                    self.output?.handle(searchError: SearchError(.noDescription))
-//                    return
-//                }
-//                self.pokemonSearchResult.description = description
-//                self.fetchSprite()
-//            }
-//        }
+        pokemonSearchResult.name = name
+        networkManager.fetchDescription(forPokemonWithName: pokemonSearchResult.name) { [weak self] (description, error) in
+            guard let self = self else { return }
+            if let error = error {
+                NSLog(error.localizedDescription)
+                self.output?.handle(searchError: error)
+            } else {
+                guard let description = description else {
+                    self.output?.handle(searchError: SearchError(.noDescription))
+                    return
+                }
+                self.pokemonSearchResult.description = description
+                self.fetchSprite()
+            }
+        }
     }
 }
 
