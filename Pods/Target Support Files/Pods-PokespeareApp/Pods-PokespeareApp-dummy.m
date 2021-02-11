2f5f1a463ab32a04d99d3c39bb75d3d4f0e1285b
Matteo Gazzato <matteo.gazzato@generali.com>
2021-02-12 00:12:57 +0100

    Remove generated Pods and workspace

--- Pods/Target Support Files/Pods-PokespeareApp/Pods-PokespeareApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PokespeareApp : NSObject
-@end
-@implementation PodsDummy_Pods_PokespeareApp
-@end
--- Pods/Target Support Files/PokespeareSDK/PokespeareSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PokespeareSDK : NSObject
-@end
-@implementation PodsDummy_PokespeareSDK
-@end
